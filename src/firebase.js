e992f9b14adbcec367c63cc61790cc75f4d18eb3
Ogechukwu Mephors <ogemephors002@gmail.com>
2022-04-18 13:21:58 +0100

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD82M6_jpVyOg2UrgK32T6e-UOptmOcHPc",
+  authDomain: "clone-52eff.firebaseapp.com",
+  projectId: "clone-52eff",
+  storageBucket: "clone-52eff.appspot.com",
+  messagingSenderId: "462862230534",
+  appId: "1:462862230534:web:6e24a579b0285511be0f87",
+};
